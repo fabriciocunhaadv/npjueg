1bb406391e89b49bd31c8ec35f0aecad4c2a099a
fabriciocunhaadv <137364160+fabriciocunhaadv@users.noreply.github.com>
2023-06-21 23:31:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-/*
+<?php
+
 Manual do NPJ
 
 CAPÍTULO I – DAS DISPOSIÇÕES GERAIS
 
 Arte. 1º. Este Regulamento rege as atividades do Núcleo de Práticas Jurídicas de Iporá (NPJ), devendo ser observado pelos estagiários e docentes-orientadores responsáveis ​​pelo (NPJ).
-*/
+
+?>
